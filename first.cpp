2b0d25e13da5f89e90ad20708ecb49149c00597e
Md Adnan qaisar <staryounger012@gmail.com>
2025-11-13 00:43:46 +0530

    Initial commit

---
+++ first.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    cout<<"Adnan"<<endl;
+    cout<<"Qaisar"<<endl;
+    
+
+    // int a = 7;
+    // int b = 15;
+
+    // int c = a + b;
+
+
+    // cout<<c<<endl;
+
+    int a;
+    int b;
+    cout<<"Enter a and b"<<endl;
+
+    // cin used for input 
+    cin>>a>>b;
+    cout<<a * b <<endl;
+ 
+    return 0;
+
+}
