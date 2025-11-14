c67c25a3fa857dc8183376947eba0daeddd7a6c3
Md Adnan qaisar <staryounger012@gmail.com>
2025-11-14 10:08:01 +0530

    Add Day-4 folder

---
+++ Day-4/Loops.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // int count = 1;
+   
+    // while(count <= 10) {
+    //     cout<< count << " ";
+    //     count++;
+
+    // }
+
+    // cout << endl;
+    // return 0;
+    
+    int  n = 100;
+    int i = 1;
+   
+    while(i <= n) {
+        cout<< i << " ";
+        i++;
+
+    }
+
+    cout << endl;
+    return 0;
+
+
+
+
+
+}
