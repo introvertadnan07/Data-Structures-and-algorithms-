c2b517fad8c29b59ca5b75e578736e96de0a27ee
Md Adnan qaisar <staryounger012@gmail.com>
2025-11-16 20:55:57 +0530

    Add Day-5 codes

---
+++ Day-5/charSquare.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+     int n;
+
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    for(int i=0; i<n; i++) {
+        char ch = 'A';
+        for(int j=0; j<n; j++) {
+            cout << ch << " ";
+        ch = ch + 1; // char + int type converstion 
+
+        }
+        cout << endl;
+        
+
+    }
+    return 0;
+
+}
