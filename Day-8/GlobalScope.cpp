1d952641a81a54a7ea9fa6bbdf7fbcfdb3222625
Md Adnan qaisar <staryounger012@gmail.com>
2025-11-22 08:33:41 +0530

    Add Day-8

---
+++ Day-8/GlobalScope.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int x = 10;
+
+void func() {
+    
+}
+
+int main() {
+
+    cout << x << endl;
+    return 0;
+}
---
+++ Day-8/Rightshift.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    cout << ( 10 >> 2) << endl;
+    cout << ( 8 >> 2) << endl;
+    cout << ( 9 >> 2) << endl;
+
+    cout << ((5 - 2) * 6) << endl;
+    return 0;
+}
