d2ec850b1937bd09b33187383581cfb1acb9bb39
Md Adnan qaisar <staryounger012@gmail.com>
2025-12-12 15:22:59 +0530

    100 days DSA

---
+++ Day-10/PopBack.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> vec;
+
+    vec.push_back(25);
+    vec.push_back(35);
+    vec.push_back(45);
+
+    cout << "after push back size = " << vec.size() << endl;
+
+    vec.pop_back();
+
+    for(int val : vec) {   
+        cout << val << endl;
+    }
+
+    return 0;
+}
---
+++ Day-10/vector.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // vector<int> vec = {1, 2, 3};
+    // vector<int> vec (5, 0);
+    // cout << vec[0] << endl;
+    // cout << vec[1] << endl;
+    // cout << vec[2] << endl;
+    // cout << vec[3] << endl;
+    // cout << vec[4] << endl;
+
+    vector<char> vec = {'a', 'b', 'c', 'd', 'e'};
+
+    for(char val : vec) {
+        cout << val << endl;
+    }
+
+    return 0;
+
+}
